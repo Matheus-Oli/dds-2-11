23e87dc5d4bda8a31af18beb75fb241ce6378ca3
filipealvesvaz <filipealvesvaz@gmail.com>
2022-03-10 17:07:12 -0300

    Initial

--- src/com/company/variavais_e_contantes_3/Exercicio03.java
+++ src/com/company/variavais_e_contantes_3/Exercicio03.java
@@ -3,6 +3,7 @@ package com.company.variavais_e_contantes_3;
 import java.util.Scanner;
 
 public class Exercicio03 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
