36012e01d3eaecf2134de47fd77262a414349e12
filipealvesvaz <filipealvesvaz@gmail.com>
2022-03-10 16:00:43 -0300

    Enunciado Reorganizado

--- src/com/company/variavais_e_contantes_3/Exercicio04.java
+++ src/com/company/variavais_e_contantes_3/Exercicio04.java
@@ -13,6 +13,35 @@ você multiplica o subtotal pelo próprio percentual e divide tudo por 100.
 Tendo o valor referente ao percentual de desconto, você vai retirar esse valor do subtotal.
 O que sobrar será o valor total final e deverá ser apresentado no console.*/
 
+import java.util.Scanner;
+
 public class Exercicio04 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor do produto: ");
+        Double valorProduto = scanner.nextDouble();
+
+        System.out.print("Digite a quantidade: ");
+        Integer quantidade = scanner.nextInt();
+
+        Double valorSubtotal = valorProduto * quantidade;
+
+        Boolean quantidadeMaiorOuIgualDez = quantidade > 10;
+
+        Double percentualDesconto = 0.0;
+
+        if (quantidadeMaiorOuIgualDez) {
+            percentualDesconto = 10.0;
+        }
+
+        Double desconto = valorSubtotal * percentualDesconto / 100;
+
+        Double valorTotalComDesconto = valorSubtotal - desconto;
+
+        System.out.println("Valor total: " + valorTotalComDesconto);
+
+        scanner.close();
+    }
 }
