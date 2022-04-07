6887a150348e01465da134ec982ae58878f45042
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-07 17:04:14 -0300

    initial

--- src/com/company/vetores/Exercicio01.java
+++ src/com/company/vetores/Exercicio01.java
@@ -1,4 +1,9 @@
 package com.company.vetores;
 
+/*
+*
+* Crie um sistema onde o usuário vai cadastrar, utilizando o console, as 5 tarefas mais importantes do dia dele.
+*  Armazene cada uma dessas tarefas em um vetor e, no final, imprima as tarefas novamente.*/
+
 public class Exercicio01 {
 }
