7135e4b5a85779925fe250d1856827ba131a4488
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-03 14:05:36 -0300

    refactor

--- src/com/company/programacao_orientada_a_objetos/exercicio02/Exercicio02.java
+++ src/com/company/programacao_orientada_a_objetos/exercicio02/Exercicio02.java
@@ -11,4 +11,12 @@ da classe Produto.
  */
 
 public class Exercicio02 {
+
+    public static void main(String[] args) {
+        Produto produto = new Produto();
+        produto.nome = "Notebook XZ10";
+        produto.quantidadeEstoque = 8;
+
+        System.out.println("Necessário repor estoque do produto " + produto.nome + "? " + produto.eNecessarioReporEstoque());
+    }
 }
