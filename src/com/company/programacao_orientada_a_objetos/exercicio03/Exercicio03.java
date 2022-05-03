52bbab495a8a7b6d97220c846eb0c9ff0fb0dc3f
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-03 15:05:07 -0300

    refactor

--- src/com/company/programacao_orientada_a_objetos/exercicio03/Exercicio03.java
+++ src/com/company/programacao_orientada_a_objetos/exercicio03/Exercicio03.java
@@ -27,5 +27,19 @@ PS: O exercício é feito em dois passos para que você perceba a importância d
 
  */
 
-public class exercicio03 {
+
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido();
+        pedido.codigo = 1;
+        pedido.subtotal = 100.0;
+        pedido.desconto = 5.0;
+
+        System.out.println("Código: " + pedido.getCodigo() + ", Subtotal: " + pedido.getSubtotal() +
+                ", Desconto: " + pedido.getDesconto() + ", Total: " + pedido.getTotal());
+
+    }
+
 }
