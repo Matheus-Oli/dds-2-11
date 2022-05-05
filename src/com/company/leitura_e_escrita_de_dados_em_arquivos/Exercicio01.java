b2632c2e3a678bb73c12912c2ad67ec2369ae9a7
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-05 17:00:42 -0300

    refactor

--- src/com/company/leitura_e_escrita_de_dados_em_arquivos/Exercicio01.java
+++ src/com/company/leitura_e_escrita_de_dados_em_arquivos/Exercicio01.java
@@ -16,5 +16,65 @@ pedindo que ele informe as atividades.
 
  */
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exercicio01 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> linhas = new ArrayList<>();
+        String tarefa;
+        int i = 0;
+        while (true) {
+            System.out.println("Tarefa " + i + ": ");
+            tarefa = scanner.nextLine();
+            if ("x".equals(tarefa)) {
+                break;
+            }
+            linhas.add(tarefa);
+            i++;
+        }
+        escreverNoArquivo("C:\\Users\\Administrador\\Desktop\\arquivo.txt", linhas);
+        scanner.close();
+        System.out.println("Fim...");
+    }
+
+    private static void escreverNoArquivo(String arquivo, ArrayList<String> linhas) throws IOException {
+        Path path = Paths.get(arquivo);
+        Files.write(path, linhas);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
