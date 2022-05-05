6d6b491e704aa7052028e14201eac2be45164851
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-05 17:21:05 -0300

    initial

---
+++ src/com/company/leitura_e_escrita_de_dados_em_arquivos/Exercicio02.java
@@ -0,0 +1,12 @@
+package com.company.leitura_e_escrita_de_dados_em_arquivos;
+
+/*
+
+Mostre a lista de tarefas para o usuário
+
+Crie um programa que imprima, no console, a lista de tarefas que você criou.
+
+ */
+
+public class Exercicio02 {
+}
