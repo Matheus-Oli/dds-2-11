c603a68f29f22f11ae65c2a8c520141683fffb59
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-10 15:57:37 -0300

    refactor

---
+++ src/com/company/orientacao_a_objetos_parte1/composicao_de_objetos/Proprietario.java
@@ -0,0 +1,10 @@
+package com.company.orientacao_a_objetos_parte1.composicao_de_objetos;
+
+public class Proprietario {
+    String nome;
+    String cpf;
+    int idade;
+    String logradouro;
+    String bairro;
+    String cidade;
+}
