ddb348bf7c018a5afd33e8d166606894ac86aa21
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-10 14:36:24 -0300

    refactor

--- src/com/company/orientacao_a_objetos_parte1/exercicio/PetShop.java
+++ src/com/company/orientacao_a_objetos_parte1/exercicio/PetShop.java
@@ -2,7 +2,7 @@ package com.company.orientacao_a_objetos_parte1.exercicio;
 /*
 instanciando e acessando atributos do objeto
 
-Utilizando o Eclipse, vamos fazer este primeiro exercícios sobre Orientação a Objetos. Fique a vontade para criar outro projeto ou utilizar um existente.
+Utilizando o IntelliJ, vamos fazer este primeiro exercícios sobre Orientação a Objetos. Fique a vontade para criar outro projeto ou utilizar um existente.
 Um pet shop te chamou para ajudar no controle dos animais que eles cuidam. Eles irão precisar de muitos serviços, o primeiro é mapear qual o nome, raça, sexo (macho ou fêmea) e idade.
 1. Crie uma classe chamada "Cachorro" e adicione os atributos para nome, raça, sexo e idade.
 2. Crie uma outra classe chamada "PetShop" para testarmos nossa classe "Cachorro". Adicione o método "main".
