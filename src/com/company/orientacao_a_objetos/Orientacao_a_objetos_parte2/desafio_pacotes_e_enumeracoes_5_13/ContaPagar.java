16fbcae6c5c9db2387d3b0a5d2cd4750925141f7
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-19 14:43:56 -0300

    Desafio Herança e Sobreposição

--- src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/desafio_pacotes_e_enumeracoes_5_13/ContaPagar.java
+++ src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/desafio_pacotes_e_enumeracoes_5_13/ContaPagar.java
@@ -1,38 +1,39 @@
 package com.company.orientacao_a_objetos.Orientacao_a_objetos_parte2.desafio_pacotes_e_enumeracoes_5_13;
 public class ContaPagar {
-    String descricao;
-    Double valor;
-    String dataVencimento;
-    Fornecedor fornecedor;
+    private String descricao;
+    private Double valor;
+    private String dataVencimento;
+    private Fornecedor fornecedor;
     private SituacaoConta situacaoConta;
     public ContaPagar() {
         this.situacaoConta = SituacaoConta.PENDENTE;
     }
-    public ContaPagar(String descricao, Double valor, String dataVencimento, Fornecedor fornecedor) {
+    public ContaPagar(Fornecedor fornecedor, String descricao, Double valor, String dataVencimento) {
+        // chamando o construtor sem parâmetros
         this();
+        this.fornecedor = fornecedor;
         this.descricao = descricao;
         this.valor = valor;
         this.dataVencimento = dataVencimento;
-        this.fornecedor = fornecedor;
     }
-    void pagar(){
-        if (SituacaoConta.PAGA.equals(this.getSituacaoConta())){
+    public void pagar() {
+        if (SituacaoConta.PAGA.equals(this.getSituacaoConta())) {
             System.out.println("Não pode pagar uma conta que já está paga: "
-                            + this.getDescricao() + ".");
+                    + this.getDescricao() + ".");
         } else if (SituacaoConta.CANCELADA.equals(this.getSituacaoConta())) {
             System.out.println("Não pode pagar uma conta que está cancelada: "
-                            + this.getDescricao() + ".");
+                    + this.getDescricao() + ".");
         } else {
-            System.out.println("Pagando conta " + this.getDescricao() +
-                    " no valor de " + this.getValor() +
-                    " e vencimento em " + this.getDataVencimento() +
-                    " do fornecedor " + this.getFornecedor().getNome() + ".");
+            System.out.println("Pagando conta " + this.getDescricao() + " no valor de "
+                    + this.getValor() + " e vencimento em " + this.getDataVencimento()
+                    + " do fornecedor " + this.getFornecedor().getNome() + ".");
+            // altera situação da conta para PAGA
             this.situacaoConta = SituacaoConta.PAGA;
         }
     }
     public void cancelar() {
-        if (SituacaoConta.PAGA.equals(this.getSituacaoConta())){
-            System.out.println("Não pode cancelar uma conta que foi paga: "
+        if (SituacaoConta.PAGA.equals(this.getSituacaoConta())) {
+            System.out.println("Não pode cancelar uma conta que já foi paga: "
                     + this.getDescricao() + ".");
         } else if (SituacaoConta.CANCELADA.equals(this.getSituacaoConta())) {
             System.out.println("Não pode cancelar uma conta que já foi cancelada: "
@@ -43,32 +44,31 @@ public class ContaPagar {
             this.situacaoConta = SituacaoConta.CANCELADA;
         }
     }
+    public Fornecedor getFornecedor() {
+        return this.fornecedor;
+    }
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
     public String getDescricao() {
-        return descricao;
+        return this.descricao;
     }
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
     public Double getValor() {
-        return valor;
+        return this.valor;
     }
     public void setValor(Double valor) {
         this.valor = valor;
     }
     public String getDataVencimento() {
-        return dataVencimento;
+        return this.dataVencimento;
     }
     public void setDataVencimento(String dataVencimento) {
         this.dataVencimento = dataVencimento;
-    }
-    public Fornecedor getFornecedor() {
-        return fornecedor;
-    }
-    public void setFornecedor(Fornecedor fornecedor) {
-        this.fornecedor = fornecedor;
     }
     public SituacaoConta getSituacaoConta() {
         return this.situacaoConta;
     }
 }
-
