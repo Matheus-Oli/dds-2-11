8321e9eabefa09efd88f43e8e2cdd1899416da34
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-24 17:04:22 -0300

    exercicio interface e polimorfismo

--- src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/_5_24_exercicio_interface_e_polimorfismo/Principal.java
+++ src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/_5_24_exercicio_interface_e_polimorfismo/Principal.java
@@ -30,4 +30,11 @@ No método main(), instanciamos uma corretora de seguros, um carro e um imóvel,
 
  */
 public class Principal {
+    public static void main(String[] args) {
+        CorretoraSeguros corretora = new CorretoraSeguros();
+        Carro meuCarro = new Carro(45000d, 2012);
+        Imovel minhaCasa = new Imovel(920000, 320);
+        corretora.fazerPropostaSeguro(meuCarro);
+        corretora.fazerPropostaSeguro(minhaCasa);
+    }
 }
