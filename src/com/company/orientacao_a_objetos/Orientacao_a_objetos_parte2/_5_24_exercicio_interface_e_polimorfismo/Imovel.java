bccb1e8dd83586d561fc5dd3c55070e9afbeb9ca
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-26 14:18:28 -0300

    reorganized packets

--- src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/_5_24_exercicio_interface_e_polimorfismo/Imovel.java
+++ src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/_5_24_exercicio_interface_e_polimorfismo/Imovel.java
@@ -1,23 +1,25 @@
 package com.company.orientacao_a_objetos.Orientacao_a_objetos_parte2._5_24_exercicio_interface_e_polimorfismo;
-public class Imovel implements Seguravel{
-    private double valorMercado;
-    private double areaConstruida;
 
-    public Imovel(double valorMercado, double areaConstruida) {
-        this.valorMercado = valorMercado;
-        this.areaConstruida = areaConstruida;
-    }
+public class Imovel implements Seguravel {
 
-    @Override
-    public String obterDescricao() {
-        return "Imóvel com área contruída de " + this.areaConstruida + "m2 e valor de mercado "
-                 + this.valorMercado;
-    }
+	private double valorMercado;
+	private int areaConstruida;
 
-    @Override
-    public double calcularValorApolice() {
-        double valorApolice = this.valorMercado * 0.003;
-        valorApolice = valorApolice + (areaConstruida * 0.5);
-        return valorApolice;
-    }
+	public Imovel(double valorMercado, int areaConstruida) {
+		this.valorMercado = valorMercado;
+		this.areaConstruida = areaConstruida;
+	}
+
+	public String obterDescricao() {
+		return "Imóvel com área construída de " + this.areaConstruida + "m2 e valor de mercado " 
+			+ this.valorMercado;
+	}
+
+	public double calcularValorApolice() {
+		// cálculos fictícios do valor de uma apólice de um imóvel
+		double valorApolice = this.valorMercado * 0.003;
+		valorApolice = valorApolice + (areaConstruida * 0.5);
+		return valorApolice;
+	}
+
 }
