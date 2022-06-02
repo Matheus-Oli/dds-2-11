055b458a056c42f9bc7eca213e457316fb9d0c62
filipealvesvaz <filipealvesvaz@gmail.com>
2022-06-01 21:00:39 -0300

    renomeando arquivo

--- src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/_5_17_desafio_heranca_e_sobreposicao/Desafio.java
+++ src/com/company/orientacao_a_objetos/Orientacao_a_objetos_parte2/_5_17_desafio_heranca_e_sobreposicao/Desafio.java
@@ -1,6 +1,6 @@
 package com.company.orientacao_a_objetos.Orientacao_a_objetos_parte2._5_17_desafio_heranca_e_sobreposicao;
 
-public class Principal {
+public class Desafio {
     public static void main(String[] args) {
         // instanciando fornecedores
         Fornecedor imobiliaria = new Fornecedor();
